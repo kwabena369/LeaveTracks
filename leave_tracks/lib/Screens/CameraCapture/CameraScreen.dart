ba320028f7b5bb8cb23ebd0324f417d5cdf33e50
TexasStudio <68285748+kwabena369@users.noreply.github.com>
2024-11-20 09:53:57 +0000

    Update CameraScreen.dart

--- leave_tracks/lib/Screens/CameraCapture/CameraScreen.dart
+++ leave_tracks/lib/Screens/CameraCapture/CameraScreen.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
-
+// hope you are fine 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
 
